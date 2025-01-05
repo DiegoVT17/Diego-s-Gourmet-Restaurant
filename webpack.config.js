17b27c7322416acca5a34fcf268007ccd93f29ce
Diego Vera <173311520+DiegoVT17@users.noreply.github.com>
2025-01-04 20:49:57 -0500

    fixed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "dist",
-    path: path.resolve(__dirname, "main.js"),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
 };
